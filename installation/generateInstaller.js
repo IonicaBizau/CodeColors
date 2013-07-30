bc49abd9d4dc7b188da223ed7743987495d4ca48
Ionica Bizau <bizauionica@yahoo.com>
2013-07-30 22:14:24 +0300

    Add some colors in the installer script.

---
+++ installation/generateInstaller.js
@@ -0,0 +1,81 @@
+var fs = require("fs");
+
+var echos = "";
+
+for (var i = 0; i < 1; ++i) {
+    echos += generateRandomEcho(27) + "\n";
+}
+
+var colors =
+   'Red="\\e[0;31m"' +
+   '\nYellow="\\e[1;33m"' +
+   '\nGreen="\\e[0;32m"' +
+   '\nBlue="\\e[0;34m"' +
+   '\nPurple="\\e[0;35m"' +
+   '\nBlack="\\e[0;30m"' +
+   '\nCyan="\\e[0;36m"' +
+   '\nBrown="\\e[0;33m"' +
+   '\nLightGray="\\e[0;37m"' +
+   '\nDarkGray="\\e[1;30m"' +
+   '\nLightBlue="\\e[1;34m"' +
+   '\nLightGreen="\\e[1;32m"' +
+   '\nLightCyan="\\e[1;36m"' +
+   '\nLightRed="\\e[1;31m"' +
+   '\nLightPurple="\\e[1;35m"' +
+   '\nWhite="\\e[1;37m"\n\n';
+
+var data = colors + echos;
+
+fs.writeFile("./installation/graphics/colors.sh", data, function (err, data) {
+    if (err) { process.exit(err); }
+});
+
+
+/*
+ *  Private
+ * */
+
+function generateRandomEcho (length) {
+    var newEcho = "echo -e \"";
+
+    for (var i = 0; i < length; ++i ) {
+        newEcho += "${" + getRandomColor() + "}" + getRandomOpacity();
+    }
+
+    newEcho += "${White}\"";
+    return newEcho;
+}
+
+
+function getRandomOpacity () {
+    var l = 0;
+    var opacity = "▒▓█";
+    l = opacity.length;
+
+    return opacity[Math.floor(Math.random() * l)];
+}
+
+function getRandomColor () {
+    var l = 0;
+    var colors = [
+        "Red",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Purple",
+        "Black",
+        "Cyan",
+        "Brown",
+        "LightGray",
+        "DarkGray",
+        "LightBlue",
+        "LightGreen",
+        "LightCyan",
+        "LightRed",
+        "LightPurple",
+        "White"
+    ];
+    l = colors.length;
+
+    return colors[Math.floor(Math.random() * l)];
+}
---
+++ installer.sh
@@ -0,0 +1,45 @@
+###########################################
+#                 COLORS                  #
+# ======================================= #
+# Black       0;30     Dark Gray     1;30 #
+# Blue        0;34     Light Blue    1;34 #
+# Gre[n       0;32     Light Gre[n   1;32 #
+# Cyan        0;36     Light Cyan    1;36 #
+# Red         0;31     Light Red     1;31 #
+# Purple      0;35     Light Purple  1;35 #
+# Brown       0;33     Yellow        1;33 #
+# Light Gray  0;37     White         1;37 #
+###########################################
+
+
+Red="\e[0;31m"
+Yellow="\e[1;33m"
+Green="\e[0;32m"
+Blue="\e[0;34m"
+Purple="\e[0;35m"
+Black="\e[0;30m"
+Cyan="\e[0;36m"
+Brown="\e[0;33m"
+LightGray="\e[0;37m"
+DarkGray="\e[1;30m"
+LightBlue="\e[1;34m"
+LightGreen="\e[1;32m"
+LightCyan="\e[1;36m"
+LightRed="\e[1;31m"
+LightPurple="\e[1;35m"
+White="\e[1;37m"
+
+mkdir installation/graphics
+node installation/generateInstaller.js
+chmod +x installation/graphics/colors.sh
+
+installation/graphics/colors.sh
+
+echo "==========================="
+echo "|        CodeColors       |"
+echo "| ----------------------- |"
+echo "|  Author:  Ionica Bizau  |"
+echo "|   (C) 2013 CodeColors   |"
+echo "==========================="
+
+installation/graphics/colors.sh
